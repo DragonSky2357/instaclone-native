3c371580a00154fde62cc7ab1526dd82c55adcf8
unknown <qkrdydals327@gmail.com>
2021-05-13 17:18:20 +0900

    update 2021 05 13

---
+++ screens/Feed.js
@@ -0,0 +1,48 @@
+import { gql, useQuery } from "@apollo/client";
+import React from "react";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import ScreenLayout from "../components/ScreenLayout";
+import { COMMENT_FRAGMENT, PHOTO_FRAGMENT } from "../fragments";
+
+const FEED_QUERY = gql`
+  query seeFeed {
+    seeFeed {
+      ...PhotoFragment
+      user {
+        username
+        avatar
+      }
+      caption
+      comments {
+        ...CommentFragment
+      }
+      createdAt
+      isMine
+    }
+  }
+  ${PHOTO_FRAGMENT}
+  ${COMMENT_FRAGMENT}
+`;
+
+export default function Feed({ navigation }) {
+  const { data, loading } = useQuery(FEED_QUERY);
+  console.log(loading, data);
+  const renderPhoto = ({ item: photo }) => {
+    return (
+      <View style={{ flex: 1 }}>
+        <Text style={{ color: "white" }}>{photo.caption}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <ScreenLayout loading={loading}>
+      <FlatList
+        data={data?.seeFeed}
+        keyExtractor={(photo) => "" + photo.id}
+        renderItem={renderPhoto}
+      />
+    </ScreenLayout>
+  );
+}
